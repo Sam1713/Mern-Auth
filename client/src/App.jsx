c20a3ed8fc5ab21b45467fe53f59d823d8d273e6
Sam1713 <samsaju399@gmail.com>
2024-04-30 10:56:22 +0530

    install ract-vite and tailwand css and created first template

---
+++ client/src/App.jsx
@@ -0,0 +1,13 @@
+// eslint-disable-next-line no-unused-vars
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      {/* Your JSX content goes here */}
+      <h1 className='text-red-500'>Hello</h1>
+    </div>
+  );
+}
+
+export default App;
