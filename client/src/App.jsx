3177943b2ff96a4e844a998fd24eb4455efe7795
Sam1713 <samsaju399@gmail.com>
2024-04-30 11:21:02 +0530

    pages added,added routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,26 @@
 // eslint-disable-next-line no-unused-vars
 import React from 'react';
-
+import { BrowserRouter,Routes,Route } from 'react-router-dom';
+import Home from './components/Home';
+import Profile from './components/Profile';
+import Signin from './components/Signin';
+import Signup from './components/Signup';
+import About from './components/About';
+import Header from './components/Header';
 function App() {
-  return (
-    <div>
-      {/* Your JSX content goes here */}
-      <h1 className='text-red-500'>Hello</h1>
-    </div>
-  );
+  return  <BrowserRouter>
+   <Routes>
+    <Route path='/' element={<Home />}/>
+    <Route path='/about' element={<About />}/>
+
+    <Route path='/profile' element={<Profile />}/>
+
+    <Route path='/signup' element={<Signup />}/>
+
+    <Route path='/signin' element={<Signin />}/>
+    </Routes>
+
+  </BrowserRouter>
 }
 
 export default App;
