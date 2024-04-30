00846b89fbb7a94e214ed91622cc5f0c6d5dc498
Sam1713 <samsaju399@gmail.com>
2024-04-30 12:41:32 +0530

    mongodb connected,environmental variable setup completed

--- api/index.js
+++ api/index.js
@@ -1,5 +1,17 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv'
+dotenv.config()
+
+mongoose.connect(process.env.MONGO).then(()=>{
+console.log("Mongo is connected")
+}).catch((error)=>{
+    console.log(error)
+})
 const app=express()
 app.listen(3000,()=>{
     console.log('server listerning on port 3000')
 })
+//Sam@123.com
+//samsaju399
+//mongodb+srv://samsaju399:<password>@mern.8k8imwr.mongodb.net/
